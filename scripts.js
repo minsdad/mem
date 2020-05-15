139d52b9bb995e4213603e56991cc1f457d76667
minsdad <65374176+minsdad@users.noreply.github.com>
2020-05-15 12:13:24 +0900

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 var num_O = 0; // 맞은 갯수
 var num_X = 0; // 틀린 갯수
 
+function Moving() {
+	location.href = "./index.html"
+}
+
 function func_toggle(myID) {
 	
 	mySpan = document.getElementById(myID);
@@ -21,17 +25,35 @@ function func_toggle(myID) {
 	}
 }
 
-function func_make() {
+function func_make(difficulty) {
 	
-	i = 0
-	
+	accum = 0;
+	rnd_value = Math.floor(Math.random()*100);
+	mySpan = document.getElementById("Q_0");
+	if (rnd_value <= difficulty) {
+		mySpan.className = "Q_box";
+		accum = accum +1;
+	}
+	else {
+		accum = 0;
+	}
+
+	i = 1;
+
 	while(document.getElementById("Q_"+i)) {
 		
-		rnd_value = Math.floor(Math.random()*100);
+		rnd_value = Math.floor(Math.random()*100);		
 		mySpan = document.getElementById("Q_"+i);
 
-		if (rnd_value <= 30) {
+		if (accum >= Math.round(difficulty/15)) {
+			accum = 0;
+		}
+		else if (rnd_value <= difficulty) {
 			mySpan.className = "Q_box";
+			accum = accum +1;
+		}
+		else {
+			accum = 0;
 		}
 
 		i = i + 1;
